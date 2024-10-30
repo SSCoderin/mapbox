5bfd2fa66488ffa78de2ab7ad6f0e24b677f84e2
Harry9021 <harryingle00@gmail.com>
2024-10-30 17:46:05 +0530

    home set to mapp

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -12,26 +12,26 @@ export default function Router() {
       <BrowserRouter>
         <Routes>
           <Route path="/maper" element={
-            <MapComponent 
-            sourceLocation={{
-              coordinates: [80.2705, 13.0843],
-              name: "Chennai"
-            }}
-            destinationLocation={{
-              coordinates: [77.2090, 28.6139],
-              name: "Delhi"
-            }}
-            intermediate1Location={{
-              coordinates: [78.4867, 17.3850],
-              name: "Hyderabad"
-            }}
-            intermediate2Location={{
-              coordinates: [75.3433, 19.8762],
-              name: "Aurangabad"
-            }}
-          />} />
-          <Route path="/" element={<Map />} />
-          <Route path="/wayport" element={<WayportLanding />} />
+            <MapComponent
+              sourceLocation={{
+                coordinates: [80.2705, 13.0843],
+                name: "Chennai"
+              }}
+              destinationLocation={{
+                coordinates: [77.2090, 28.6139],
+                name: "Delhi"
+              }}
+              intermediate1Location={{
+                coordinates: [78.4867, 17.3850],
+                name: "Hyderabad"
+              }}
+              intermediate2Location={{
+                coordinates: [75.3433, 19.8762],
+                name: "Aurangabad"
+              }}
+            />} />
+          <Route path="/access" element={<Map />} />
+          <Route path="/" element={<WayportLanding />} />
         </Routes>
       </BrowserRouter>
     </>
